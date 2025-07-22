6d18af51f624fe72daae0e4e36c73b8b2b7f9fd3
Harshwardhan Khandelwal <khandelwalharsh0024@gmail.com>
2025-07-22 18:13:09 +0530

    revert

--- Backend/server.js
+++ Backend/server.js
@@ -18,21 +18,19 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({extended:true}));
 app.use(cors({
-    origin:'http://localhost:1000',
+    origin:'http://localhost:5173',
     credentials:true,
     methods:['PUT','POST','GET','DELETE']
 }))
 
 
 
-// Deployment part...
-const _dirname = path.resolve();
-app.use(express.static(path.join(_dirname,'/Frontend/dist')));
-app.get('*',(_,res)=>{
-       res.sendFile(path.resolve(_dirname,'Frontend','dist','index.html'))
-})
-
-
+// // Deployment part...
+// const _dirname = path.resolve();
+// app.use(express.static(path.join(_dirname,'/Frontend/dist')));
+// app.get('*',(_,res)=>{
+//        res.sendFile(path.resolve(_dirname,'Frontend','dist','index.html'))
+// })
 
 mongoose.connect(process.env.MONGO_URL,{dbName:'Job_Portal'}).then(()=>console.log('MongoDB connected')).catch(()=>console.log('Errro Occured'));
 const port = process.env.PORT || 4000;
